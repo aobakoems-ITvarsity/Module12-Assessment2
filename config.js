abb4a60709aecc7c51831e123ef5bd64f4710b77
lebogang sithole <aobakoems@yahoo.com>
2022-06-08 14:45:19 +0200

    fixed

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
-let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";// location of the server
+let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
 let apiKey = checkApiKey();
 
-function checkApiKey() {// sends the user to another page if their api key is not entered
+function checkApiKey() {
     if (!localStorage.getItem("apiKey")) {
         window.open("enter-api-key.html", "_self");
     }
